3f2cdaa4ecd73038a711da4195e14670ca09b6c0
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-12 19:10:02 +0300

    Add files via upload

---
+++ 02. Division/Program.cs
@@ -0,0 +1,32 @@
+﻿int number = int.Parse(Console.ReadLine());
+int divisibleMax = 0;
+
+if (number % 10 == 0)
+{
+    divisibleMax = 10;
+}
+else if (number % 7 == 0)
+{
+    divisibleMax = 7;
+}
+else if (number % 6 == 0)
+{
+    divisibleMax = 6;
+}
+else if (number % 3 == 0)
+{
+    divisibleMax = 3;
+}
+else if (number % 2 == 0)
+{
+    divisibleMax = 2;
+}
+
+if (divisibleMax != 0)
+{
+    Console.WriteLine($"The number is divisible by {divisibleMax}");
+}
+else
+{
+    Console.WriteLine($"Not divisible");
+}
