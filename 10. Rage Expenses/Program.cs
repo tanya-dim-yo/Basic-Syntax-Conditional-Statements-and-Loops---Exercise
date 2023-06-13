748cfc26564bfb80ca1a8a023792b5b5ab65f0db
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-14 02:00:55 +0300

    Add files via upload

---
+++ 10. Rage Expenses/Program.cs
@@ -0,0 +1,32 @@
+﻿int lostGames = int.Parse(Console.ReadLine());
+double headsetPrice = double.Parse(Console.ReadLine());
+double mousePrice = double.Parse(Console.ReadLine());
+double keyboardPrice = double.Parse(Console.ReadLine());
+double displayPrice = double.Parse(Console.ReadLine());
+
+int trashedHeadsetNumber = 0;
+int trashedMouseNumber = 0;
+int trashedKeyboardNumber = 0;
+int trashedDisplayNumber = 0;
+
+for (int i = 1; i <= lostGames; i++)
+{
+    if (i % 2 == 0)
+    {
+        trashedHeadsetNumber++;
+    }
+
+    if (i % 3 == 0)
+    {
+        trashedMouseNumber++;
+    }
+
+    if (i % 2 == 0 && i % 3 == 0)
+    {
+        trashedKeyboardNumber++;
+    }
+}
+
+trashedDisplayNumber = trashedKeyboardNumber / 2;
+double totalSum = (trashedHeadsetNumber * headsetPrice) + (trashedMouseNumber * mousePrice) + (trashedKeyboardNumber * keyboardPrice) + (trashedDisplayNumber * displayPrice);
+Console.WriteLine($"Rage expenses: {totalSum:F2} lv.");
