66c7d201ab43efd9bdda370a1496bad648bcf550
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-14 02:00:15 +0300

    Add files via upload

---
+++ 09. Padawan Equipment/Program.cs
@@ -0,0 +1,20 @@
+﻿float moneyProvided = float.Parse(Console.ReadLine());
+float studentsNumber = float.Parse(Console.ReadLine());
+float lightsaberPrice = float.Parse(Console.ReadLine());
+float robePrice = float.Parse(Console.ReadLine());
+float beltPrice = float.Parse(Console.ReadLine());
+
+float lightsabersSum = lightsaberPrice * (int)Math.Ceiling(studentsNumber + studentsNumber * 0.1);
+float robesSum = robePrice * studentsNumber;
+float beltSum = beltPrice * (int)Math.Ceiling(studentsNumber - (studentsNumber / 6));
+
+float totalSum = lightsabersSum + robesSum + beltSum;
+
+if (totalSum <= moneyProvided)
+{
+    Console.WriteLine($"The money is enough - it would cost {totalSum:F2}lv.");
+}
+else
+{
+    Console.WriteLine($"John will need {totalSum - moneyProvided:F2}lv more.");
+}
