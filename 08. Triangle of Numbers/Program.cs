d3f6d1de6ab94da5f75770eae78c08b9a45643e5
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-14 01:59:23 +0300

    Add files via upload

---
+++ 08. Triangle of Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int rows = 1; rows <= n; rows++)
+{
+    for (int cols = 1; cols <= rows; cols++)
+    {
+        Console.Write(rows + " ");
+    }
+    
+    Console.WriteLine();
+}
