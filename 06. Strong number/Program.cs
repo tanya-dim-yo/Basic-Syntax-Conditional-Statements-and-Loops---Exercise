fe02152a0e81aeebe1f017e64598b8a1afe2a704
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-13 05:13:03 +0300

    Add files via upload

---
+++ 06. Strong number/Program.cs
@@ -0,0 +1,26 @@
+﻿int n = int.Parse(Console.ReadLine());
+int input = n;
+int sum = 0;
+
+while (n > 0)
+{
+    int number = n % 10;
+    int fact = 1;
+
+    for (int i = 1; i <= number; i++)
+    {
+        fact *= i;
+    }
+
+    sum += fact;
+    n /= 10;
+}
+
+if (sum == input)
+{
+    Console.WriteLine($"yes");
+}
+else
+{
+    Console.WriteLine($"no");
+}
