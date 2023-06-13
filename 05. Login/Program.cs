0f1268e388a9198b50922612095ff53104f88fbd
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-13 05:12:26 +0300

    Add files via upload

---
+++ 05. Login/Program.cs
@@ -0,0 +1,28 @@
+﻿string username = Console.ReadLine();
+string password = Console.ReadLine();
+string usernameReversed = string.Empty;
+int counterUnsuccessful = 0;
+
+for (int i = username.Length - 1; i >= 0; i--)
+{
+    usernameReversed += username[i];
+}
+
+while (password != usernameReversed)
+{
+    Console.WriteLine($"Incorrect password. Try again.");
+    counterUnsuccessful++;
+
+    if (counterUnsuccessful >= 3)
+    {
+        Console.WriteLine($"User {username} blocked!");
+        break;
+    }
+
+    password = Console.ReadLine();
+}
+
+if (password == usernameReversed)
+{
+    Console.WriteLine($"User {username} logged in.");
+}
