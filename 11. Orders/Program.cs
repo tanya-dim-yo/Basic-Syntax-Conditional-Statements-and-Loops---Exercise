a9a887895202922143745dcccd5635e1771ad90f
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-15 04:06:35 +0300

    Add files via upload

---
+++ 11. Orders/Program.cs
@@ -0,0 +1,15 @@
+﻿int ordersNumber = int.Parse(Console.ReadLine());
+double totalSum = 0;
+
+for (int i = 1; i <= ordersNumber; i++)
+{
+    double pricePerCapsule = double.Parse(Console.ReadLine());
+    int daysInMonth = int.Parse(Console.ReadLine());
+    int capsulesCount = int.Parse(Console.ReadLine());
+
+    double sum = (daysInMonth * capsulesCount) * pricePerCapsule;
+    Console.WriteLine($"The price for the coffee is: ${sum:F2}");
+    totalSum += sum;
+}
+
+Console.WriteLine($"Total: ${totalSum:F2}");
